e9a1bb7037a4bbbdc81e1e6cf3d5d2483cc884ae
FelixDubois <services@felixdubois.dev>
2023-05-19 15:33:39 +0200

    First Commit

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,9 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  -- { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.cmake" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
